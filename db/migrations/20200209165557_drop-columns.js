7845648628a3648f0fc89e336a2c94d09f02bf05
Colin Turing <colin.koga@gmail.com>
2020-02-09 17:33:01 -0700

    Drop palette id columns from projects and added them again with onDelete(CASCADE).  migration was successful but still getting violates foreign key constraint in test

---
+++ db/migrations/20200209165557_drop-columns.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex) {
+  return knex.schema.table('projects', table => {
+    table.dropColumn('palette1_id')
+    table.dropColumn('palette2_id')
+    table.dropColumn('palette3_id')
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.table('projects', table => {
+    table.integer('palette1_id').unsigned();
+    table.foreign('palette1_id').references('palettes.id');
+    table.integer('palette2_id').unsigned();
+    table.foreign('palette2_id').references('palettes.id');
+    table.integer('palette3_id').unsigned();
+    table.foreign('palette3_id').references('palettes.id');
+  })
+};
